600eca7f54157bb31c9889ec5ca5af929b662a97
FarshidRA <40944005+FarshidRA@users.noreply.github.com>
2025-11-21 14:27:13 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-const sb = supabase.createClient(supabaseUrl, supabaseKey);
-
-sb.channel('realtime:public:messages')
-  .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'messages' }, payload => {
-    console.log(payload.new.text);
-  })
-  .subscribe();
